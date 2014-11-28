2bd8e0cf2f94b2f4f17f61c51fa51f06259acf02
Arun M Kumar <arunkr24@hotmail.com>
2014-11-28 19:23:42 +0530

    out device type added

--- Comm.cpp
+++ Comm.cpp
@@ -21,12 +21,15 @@ Comm::Comm(){
 	outDevice.baudRate = COMM_NO_BAUD;
 }
 
-Comm::Comm(device_t inDeviceType, baud_t inBaud, device_t outCommType, baud_t outBaud ){
+Comm::Comm(device_t inDeviceType, baud_t inBaud, device_t outDeviceType, baud_t outBaud ){
 	/*
 		Function to initialize the Comm module
 		both the In nad Out devices are set.
 		PS: They have to be initialized here
 	*/
+
+
+	// %%%%%%%%%%%%%%%%%% IN DEVICE %%%%%%%%%%%%%%%%%%%%%%%
 	if (COMM_TYPE_I2C == inDeviceType){
 		inDevice.deviceType = inDeviceType;
 		setCommStatus(COMM_INIT);
@@ -42,15 +45,51 @@ Comm::Comm(device_t inDeviceType, baud_t inBaud, device_t outCommType, baud_t ou
 		Serial.begin(UART_BAUD_RATE);
 	}
 
-	else if (COMM_TYPE_BLE == commType){
+	else if (COMM_TYPE_BLE == inDeviceType){
 		inDevice.deviceType = inDeviceType;
 		setCommStatus(COMM_INIT);
 		//BLE initioalization code
 
 	}
 
-	else if (COMM_TYPE_SPI == commType){
+	else if (COMM_TYPE_SPI == inDeviceType){
 		inDevice.deviceType = inDeviceType;
+		setCommStatus(COMM_INIT);
+		//SPI Initialization code
+	}
+
+	else{
+		// error handling
+		inDevice.deviceType = COMM_NOT_DEFINED;
+		setCommStatus(COMM_NOT_INIT);
+
+	}
+
+	// %%%%%%%%%%%%%%%%%% OUT DEVICE %%%%%%%%%%%%%%%%%%%%%%%
+	if (COMM_TYPE_I2C == outDeviceType){
+		inDevice.deviceType = outDeviceType;
+		setCommStatus(COMM_INIT);
+		// I2C Initialization code
+		Wire.begin(SRC_I2C_ADDRESS);	// current devices I2C address
+
+	}
+
+	else if (COMM_TYPE_UART == outDeviceType){
+		inDevice.deviceType = outDeviceType;
+		setCommStatus(COMM_INIT);
+		// Serial Initialization code
+		Serial.begin(UART_BAUD_RATE);
+	}
+
+	else if (COMM_TYPE_BLE == outDeviceType){
+		inDevice.deviceType = outDeviceType;
+		setCommStatus(COMM_INIT);
+		//BLE initioalization code
+
+	}
+
+	else if (COMM_TYPE_SPI == outDeviceType){
+		inDevice.deviceType = outDeviceType;
 		setCommStatus(COMM_INIT);
 		//SPI Initialization code
 	}
