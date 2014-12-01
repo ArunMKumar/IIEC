abfa5e39de4878b5b112b6c2912c18d1379ddf6b
Arun M Kumar <arunkr24@hotmail.com>
2014-12-01 07:25:37 +0530

    started with creating node instances, need to finish the protocol now

--- Project.cpp
+++ Project.cpp
@@ -11,6 +11,7 @@ Arun M Kumar						30 Nov 2014
 
 #include "Project.h"
 #include "Load.h"
+#include "Node.h"
 
 /*
 	First we need to initialize the loads.
@@ -23,6 +24,13 @@ Load Load1(LOAD_0R, LOAD_0W, LOAD_ID0, LOAD_PRIO_DEFAULT, LOAD_CLASS_DEFAULT);
 Load Load2(LOAD_1R, LOAD_1W, LOAD_ID1, LOAD_PRIO_DEFAULT, LOAD_CLASS_DEFAULT);
 Load Load3(LOAD_2R, LOAD_2W, LOAD_ID2, LOAD_PRIO_DEFAULT, LOAD_CLASS_DEFAULT);
 Load Load4(LOAD_3R, LOAD_3W, LOAD_ID3, LOAD_PRIO_DEFAULT, LOAD_CLASS_DEFAULT);
+
+
+/*
+	Creating the Node instanc here
+*/
+
+Node thisNode(NODE_ID, NODE_PID);
 
 
 /*
